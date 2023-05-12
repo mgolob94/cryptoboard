015afbc4a27c8618b48ca673341e4da5f985418e
mgolob94 <marcel.golob@gmail.com>
2023-05-12 20:37:23 +0200

    conf

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  distDir: "build",
+  trailingSlash: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
